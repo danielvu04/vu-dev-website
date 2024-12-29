31856d5584372686826b410a0338de8b6ca9630b
Daniel <vu000194@umn.edu>
2024-12-29 01:41:24 -0600

    Mobile Website added to slideshow

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import Slider from 'react-slick';
 import '../styles/Hero.css';
 import droneImage from '../assets/images/DroneSim.png';
+import websiteImage from '../assets/images/websiteportfolio.png';
 
 function Hero() {
+
+  // Detect screen width
+  const isMobile = window.innerWidth <= 768; // Define mobile breakpoint
+
   // Slider settings
   const settings = {
-    dots: true, // Shows dots below the slideshow
+    dots: !isMobile, // Show dots only if not mobile
     infinite: true, // Infinite loop
     speed: 500, // Transition speed
     slidesToShow: 1, // Number of slides to show at once
     slidesToScroll: 1, // Number of slides to scroll at once
-    arrows: true, // Arrows for navigation
+    arrows: !isMobile, // Arrows for navigation
   };
 
   const navigate = useNavigate(); // Hook to navigate between pages
@@ -25,7 +30,7 @@ function Hero() {
             <img src={droneImage} alt="Drone Delivery Simulation" />
           </div>
           <div>
-            <img src="https://via.placeholder.com/1200x600" alt="Slide 2" />
+            <img src={websiteImage} alt="Portfolio Website" />
           </div>
           <div>
             <img src="https://via.placeholder.com/1200x600" alt="Slide 3" />
